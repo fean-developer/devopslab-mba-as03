86fe54ff000a81cc339d1b37f92e86932a6940e0
Francisco Nascimento <d19sp.webdeveloper@outlook.com>
2022-02-05 19:03:38 -0300

    Alterando o Texto

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def pagina_inicial():
-    return "APP FOR PYTHON AND FLASK"
+    return "APP PYTHON AND FLASK "
 
 if __name__ == '__main__':
     port = os.getenv('PORT')
