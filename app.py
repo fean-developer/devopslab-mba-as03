e5fb9268c46d758b8b74dea5350d7437d4fff522
Francisco Nascimento <77283761+fean-developer@users.noreply.github.com>
2022-02-05 19:09:28 -0300

    UPDATE TEXT CONTENT

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def pagina_inicial():
-    return "APP PYTHON AND FLASK "
+    return "APP PYTHON AND FLASK FOR ACADEMIC"
 
 if __name__ == '__main__':
     port = os.getenv('PORT')
